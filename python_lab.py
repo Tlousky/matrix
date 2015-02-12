51890e53e19fb850d3628487111d106a21e5d44b
Tlousky <tlousky@gmail.com>
2015-02-12 16:55:24 +0200

    More exercieses solved

--- python_lab.py
+++ python_lab.py
@@ -61,7 +61,7 @@ Y2 = { 4, 8, 16 }
 S = {1, 2, 3, 4}
 T = {3, 4, 5, 6}
 # Replace { ... } with a one-line set comprehension that evaluates to the intersection of S and T
-S_intersect_T = { ... }
+S_intersect_T = { x for x in S if x in T }
 
 
 
@@ -70,13 +70,13 @@ list_of_numbers = [20, 10, 15, 75]
 # Replace ... with a one-line expression that evaluates to the average of list_of_numbers.
 # Your expression should refer to the variable list_of_numbers, and should work
 # for a list of any length greater than zero.
-list_average = ...
+list_average = sum( list_of_numbers ) / len( list_of_numbers )
 
 
 
 ## 11: (Task 11) Cartesian-product comprehension
 # Replace ... with a double list comprehension over ['A','B','C'] and [1,2,3]
-cartesian_product = ...
+cartesian_product = [ [x,y] for x in ['A','B','C'] for y in [1,2,3] ]
 
 
 
@@ -84,21 +84,21 @@ cartesian_product = ...
 LofL = [[.25, .75, .1], [-1, 0], [4, 4, 4, 4]]
 # Replace ... with a one-line expression of the form sum([sum(...) ... ]) that
 # includes a comprehension and evaluates to the sum of all numbers in all the lists.
-LofL_sum = ...
+LofL_sum = sum( [ sum(x) for x in LofL ] )
 
 
 
 ## 13: (Task 13) Three-element tuples summing to zero
 S = {-4, -2, 1, 2, 5, 0}
 # Replace [ ... ] with a one-line list comprehension in which S appears
-zero_sum_list = [ ... ]
+zero_sum_list = [ (x,y,z) for x in S for y in S for z in S if sum([x,y,z]) == 0 ]
 
 
 
 ## 14: (Task 14) Nontrivial three-element tuples summing to zero
 S = {-4, -2, 1, 2, 5, 0}
 # Replace [ ... ] with a one-line list comprehension in which S appears
-exclude_zero_list = [ ... ]
+exclude_zero_list = [ (x,y,z) for x in S for y in S for z in S if sum([x,y,z]) == 0 and x != 0 and y != 0 and z != 0]
 
 
 
